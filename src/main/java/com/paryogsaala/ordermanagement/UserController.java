7c624c2f9022dd7c90209e6fe4382ece6e4f59b2
jonishprajapati <jonishprajapati6@gmail.com>
2026-08-14 01:09:27 -0500

    Added Response entity to each end points to receive valid http status code.

--- src/main/java/com/paryogsaala/ordermanagement/UserController.java
+++ src/main/java/com/paryogsaala/ordermanagement/UserController.java
@@ -2,6 +2,8 @@ package com.paryogsaala.ordermanagement;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -14,19 +16,25 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
+
+    public ResponseEntity<List<User>> getAllUsers() {
+
+        //return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @PostMapping("/api/users")
-    public String createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
         userService.addUser(user);
-        return "User created!";
+        return new ResponseEntity<>("User created!", HttpStatus.OK);
     }
     @GetMapping("/api/users/{id}")
-    public User getUserById(@PathVariable Long id) {
-
-        return userService.getUserById(id);
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(userService.getUserById(id));
     }
 
 }
