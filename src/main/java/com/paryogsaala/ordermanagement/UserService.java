8c8eba5656219807d1514f1118a7c93314937fd2
jonishprajapati <jonishprajapati6@gmail.com>
2026-08-14 00:44:46 -0500

    auto increment of ID

--- src/main/java/com/paryogsaala/ordermanagement/UserService.java
+++ src/main/java/com/paryogsaala/ordermanagement/UserService.java
@@ -7,15 +7,16 @@ import java.util.List;
 
 @Service
 public class UserService {
+    private Long id=1L;
     public List<User> userList = new ArrayList<>();
 
     public List<User> getAllUsers() {
         return userList;
     }
 
-    public List<User> addUser(User user) {
+    public void addUser(User user) {
+        user.setId(id++);
         userList.add(user);
-        return userList;
     }
 
 }
