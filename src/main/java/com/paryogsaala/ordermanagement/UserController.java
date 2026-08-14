6a08f7fefb5cc5415bea2de0788415cbdccd8cb4
jonishprajapati <jonishprajapati6@gmail.com>
2026-08-14 00:53:54 -0500

    Path variable added so user can be accessed by their Id

--- src/main/java/com/paryogsaala/ordermanagement/UserController.java
+++ src/main/java/com/paryogsaala/ordermanagement/UserController.java
@@ -2,10 +2,7 @@ package com.paryogsaala.ordermanagement;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,4 +23,10 @@ public class UserController {
         userService.addUser(user);
         return "User created!";
     }
+    @GetMapping("/api/users/{id}")
+    public User getUserById(@PathVariable Long id) {
+
+        return userService.getUserById(id);
+    }
+
 }
